b21eaac480cdf3fecbe719c71cdc7bbefc1c5b40
tchama <jaum_mathieu@collectifs.net>
2024-09-22 12:35:18 +0200

    1 change by Nuxt Studio

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,9 +1,14 @@
 export default defineAppConfig({
   // Configure Content Wind here
   cover: '/cover.jpg',
+
   socials: {
     twitter: 'Atinux',
     github: 'Atinux/content-wind',
     mastodon: '@atinux@webtoo.ls'
+  },
+
+  nuxtIcon: {
+    size: '1.2em'
   }
 })
